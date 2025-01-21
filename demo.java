fe137a6b65534adc60ea97d6903e3cdab90b1a18
subhraJ-singh <subhra.jyotisingh@flipkart.com>
2025-01-21 12:44:40 +0530

    Delete demo.java

--- demo.java
+++
@@ -1,8 +0,0 @@
-public class demo
- { 
-    public static void main(String[] args) {
-        // Print a greeting message to the console
-        System.out.println("Hello\nSubhraaaaaaaaa !");
-    } 
-}
-
